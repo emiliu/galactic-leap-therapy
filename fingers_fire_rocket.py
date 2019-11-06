4418bff18c178ee3792fc89f2c9145cb42c5f1c0
Julie Vaughn <juliev@mit.edu>
2019-11-05 21:57:12 -0500

    Graphics with notes added

--- fingers_fire_rocket.py
+++ fingers_fire_rocket.py
@@ -19,6 +19,60 @@ from kivy.core.image import Image
 
 import numpy as np
 
+from common.audio import Audio
+from common.synth import Synth
+from common.note import NoteGenerator, Envelope
+from common.wavegen import WaveGenerator, SpeedModulator
+from common.wavesrc import WaveBuffer, WaveFile, make_wave_buffers
+
+FRAME_RATE = 44100
+SF_PATH = '../unit5/data/FluidR3_GM.sf2'
+
+class NoteCluster(object):
+    def __init__(self, synth, channel, notes):
+        super(NoteCluster, self).__init__()
+        if type(notes) is int:
+            notes = [notes]
+        self.notes = tuple(notes)
+        self.synth = synth
+        self.channel = channel
+
+    def noteon(self):
+        for note in self.notes:
+            vel = 80
+            self.synth.noteon(self.channel, note, vel)
+    def noteoff(self):
+        for note in self.notes:
+            self.synth.noteoff(self.channel, note)
+
+
+
+class NoteSequencer(object):
+    def __init__(self, synth, notes, channel=0, program=(0, 0)):
+        super(NoteSequencer, self).__init__()
+        self.notes = [NoteCluster(synth, channel, note) for note in notes]
+        self.synth = synth
+        self.channel = channel
+        self.program = program
+        self.synth.program(self.channel, self.program[0], self.program[1])
+
+        self.index = 0
+        self.map = dict()
+        self.stopped = False
+
+    def noteon(self, keycode):
+        if self.index >= len(self.notes):
+            self.stopped = True
+            return
+        self.notes[self.index].noteon()
+        self.map[keycode] = self.notes[self.index]
+        self.index += 1
+
+    def noteoff(self, keycode):
+        if self.stopped: return
+        self.map[keycode].noteoff()
+        del self.map[keycode]
+
 
 
 # for use with scale_point
@@ -53,6 +107,15 @@ class FingerWidget5(BaseWidget) :
         self.bind(size=self.update_bg)
         #bind background to size of window
 
+
+        self.audio = Audio(2)
+        self.synth = Synth(SF_PATH)
+        self.audio.set_generator(self.synth)
+
+        self.notes = NoteSequencer(self.synth, [69, 72, 76, 81, [83, 68], 76, 72, 83, [84, 67], 76, 72, 84, [78, 66], 74, 69, 74, [76, 65], 72, 69, 72, [76, 65], 72, 69, [71, 50], [72, 45], [72, 45]])
+
+
+
         
         self.objects = AnimGroup()
 
@@ -80,7 +143,6 @@ class FingerWidget5(BaseWidget) :
             self.finger_disp.append(disp)
 
 
-        self.ps = None
 
 
        
@@ -101,6 +163,35 @@ class FingerWidget5(BaseWidget) :
         # self.canvas.add(text)
         #self.objects.on_update()
 
+        self.audio.on_update()
+
+        # threshold = .2
+
+        # print("norm pts", norm_pts)
+
+        # if norm_pts[2][0] > threshold:
+        #     #print("active")
+        #     self.active_status = True
+        #     #self.hand_disp.set_color((0,0,1))
+            
+        # else:
+        #     self.active_status = False
+        #     #self.hand_disp.set_color((1,0,0))
+
+
+        # thumb_index = sum((norm_pts[0]-norm_pts[1])**2)
+        # #for detecting flexion exercise
+        # if thumb_index < .05 and self.active_status:
+        #     self.rockets[0].flame_on()
+        #     self.notes.noteon(1)
+        #     self.active_status = False
+
+        
+        #print(norm_pt[2])
+      
+
+       
+
         
 
     #proxy while we work on gesture detection
@@ -114,6 +205,7 @@ class FingerWidget5(BaseWidget) :
             # #self.rockets[gesture_proxy].color = 
             # self.rockets[gesture_proxy].shoot_laser()
             self.rockets[gesture_proxy-1].flame_on()
+            self.notes.noteon(keycode[1])
 
             # if gesture_proxy-1 == 0:
             #     print("first rocket")
@@ -135,6 +227,7 @@ class FingerWidget5(BaseWidget) :
         if gesture_proxy:
             #stop rocket shooting
             self.rockets[gesture_proxy-1].flame_off()
+            self.notes.noteoff(keycode[1])
 
 
 
